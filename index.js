dea5b98830ac0c35c104c11742ba6d240750e915
juliotagle <161630273+juliotagle@users.noreply.github.com>
2024-09-09 10:19:03 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+<!document html>
+<html lang="en">
+<head>
+  <mata charset="UTF8">
+  <mata http-equiv="x_UA-compatible" content "IE=edge">
+  <title>DOCument</title>
+  <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <script src="index.js"><</script>
